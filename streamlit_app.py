527849b38583dad937638597001007f971d798e4
Amarinfo456 <142316276+Amarinfo456@users.noreply.github.com>
2023-08-15 18:41:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Bluberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
