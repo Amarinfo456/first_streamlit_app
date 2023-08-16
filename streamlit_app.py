c6052ccb4f7519019da26c25d241b6a33ab4078b
Amarinfo456 <142316276+Amarinfo456@users.noreply.github.com>
2023-08-17 02:44:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,6 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Tost')
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
