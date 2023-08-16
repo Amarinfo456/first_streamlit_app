c0c484bc3ae60d9510e87e9119321799d703717c
Amarinfo456 <142316276+Amarinfo456@users.noreply.github.com>
2023-08-17 03:05:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,9 @@ streamlit.text('🥑🍞Avocado Tost')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/daw/fruit_macros.txt")
+
+
 
 
 
