12ba5c460ced4545c32679e2ea04177be2b934e1
Amarinfo456 <142316276+Amarinfo456@users.noreply.github.com>
2023-08-17 05:36:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,6 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('what fruit would you like information about?','Kiwi')
 
 streamlit.write('The user entered',fruit_choice
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+                
